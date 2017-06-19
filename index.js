beefc4aaaa1b620c96be74851139eb9398d9e590
Foley <christopher.foley@vanderbilt.edu>
2017-06-19 13:27:39 -0500

    Replaced var with const and improved flow using Promise.all

--- index.js
+++ index.js
@@ -5,56 +5,69 @@ const baseUrl = bungieUrl + '/platform/Destiny';
 const destiny = require('./src/DestinyClient.js');
 const destinyClient = new destiny.DestinyClient( apiKey, baseUrl );
 
-var membershipType = '1';
+const membershipType = '1';
 
 destinyClient.search(membershipType, 'Carsten').then( function( response ){
     console.log('Got a response!');
-    var membership = response.data.Response[0];
-    var membershipId = membership.membershipId;
+    const membership = response.data.Response[0];
+    const membershipId = membership.membershipId;
     console.log('Users membership id: ' + membershipId );
 
     destinyClient.getAccountSummary( membershipType, membershipId ).then( function( response ){
         console.log('Got Membership Summary');
-        var characters = response.data.Response.data.characters;
-        var grimoireScore = 4615;
+        const characters = response.data.Response.data.characters;
+        const grimoireScore = 4615;
         console.log( 'User has ' + characters.length + ' characters and a grimoire score of ' + grimoireScore );
 
         destinyClient.definitions = true;
 
-        characters.forEach(function(character) {
-            console.log( 'Character ' + character.characterBase.characterId + ' light level is: ' + character.characterBase.powerLevel );
+        const characterId1 = characters[0].characterBase.characterId;
+        const characterId2 = characters[1].characterBase.characterId;
+        const characterId3 = characters[2].characterBase.characterId;
 
-            var params = { mode: 'Raid', count: '10', page: '0'}
-            destinyClient.getActivityHistory( membershipType, membershipId, character.characterBase.characterId, params ).then(function( response ){
-                console.log( 'Raid History for ' + character.characterBase.characterId );
-                var raidActivities = response.data.Response.data.activities;
-                var raidDefinitions = response.data.Response.definitions;
+        const params = { mode: 'Raid', count: '10', page: '0'};
+
+        const activitySummary1Promise = destinyClient.getActivityHistory( membershipType, membershipId, characterId1, params );
+        const activitySummary2Promise = destinyClient.getActivityHistory( membershipType, membershipId, characterId2, params );
+        const activitySummary3Promise = destinyClient.getActivityHistory( membershipType, membershipId, characterId3, params );
+
+        Promise.all( [activitySummary1Promise, activitySummary2Promise, activitySummary3Promise] ).then( characterActivities => {
+            console.log('All promises resolved');
+
+            characterActivities.forEach ( characterActivity => {
+                const raidActivities = characterActivity.data.Response.data.activities;
+                const raidDefinitions = characterActivity.data.Response.definitions;
 
                 raidActivities.forEach( function( raidActivity ){
-                    var raidReferenceId = raidActivity.activityDetails.referenceId;
+                    const raidReferenceId = raidActivity.activityDetails.referenceId;
                     console.log( 'You ran ' + raidDefinitions.activities[raidReferenceId].activityName + ' on ' + raidActivity.period );
                 })
+            })
 
-            });
+        } );
 
-            destinyClient.getCharacterSummary( membershipType, membershipId, character.characterBase.characterId ).then( function( response ) {
-                var characterResponse = response.data.Response.data
-                var definitions = response.data.Response.definitions;
+        const characterSummary1Promise = destinyClient.getCharacterSummary( membershipType, membershipId, characterId1 );
+        const characterSummary2Promise = destinyClient.getCharacterSummary( membershipType, membershipId, characterId2 );
+        const characterSummary3Promise = destinyClient.getCharacterSummary( membershipType, membershipId, characterId3 );
 
-                var genderHash = characterResponse.characterBase.genderHash;
-                var raceHash = characterResponse.characterBase.raceHash;
-                var classHash = characterResponse.characterBase.classHash;
+        Promise.all( [characterSummary1Promise, characterSummary2Promise, characterSummary3Promise]).then( characterSummaries => {
+            characterSummaries.forEach( characterSummary => {
+                const characterResponse = characterSummary.data.Response.data
+                const definitions = characterSummary.data.Response.definitions;
 
-                var genderName = definitions.genders[genderHash].genderName;
-                var raceName = definitions.races[raceHash].raceName;
-                var className = definitions.classes[classHash].className;
+                const genderHash = characterResponse.characterBase.genderHash;
+                const raceHash = characterResponse.characterBase.raceHash;
+                const classHash = characterResponse.characterBase.classHash;
 
+                const genderName = definitions.genders[genderHash].genderName;
+                const raceName = definitions.races[raceHash].raceName;
+                const className = definitions.classes[classHash].className;
 
                 if ( className == 'Titan') {
-                    destinyClient.getCharacterInventorySummary( membershipType, membershipId, character.characterBase.characterId ).then( function( response ){
+                    destinyClient.getCharacterInventorySummary( membershipType, membershipId, characterResponse.characterBase.characterId ).then( function( response ){
                         console.log( 'Got Inventory for Titan');
-                        var itemDefinitions = response.data.Response.definitions;
-                        var items = response.data.Response.data.items;
+                        const itemDefinitions = response.data.Response.definitions;
+                        const items = response.data.Response.data.items;
 
                         items.forEach( function( item ) {
                             console.log( 'Item is ' + itemDefinitions.items[item.itemHash].itemName );
@@ -65,9 +78,11 @@ destinyClient.search(membershipType, 'Carsten').then( function( response ){
                 }
 
                 console.log( 'Got character summary for ' + genderName + ' ' + raceName + ' ' + className );
-                console.log( 'Emblem Url: ' + bungieUrl + response.data.Response.data.emblemPath);
-                console.log( 'Icon Url: ' + bungieUrl + response.data.Response.data.backgroundPath);
+                console.log( 'Emblem Url: ' + bungieUrl + characterSummary.data.Response.data.emblemPath);
+                console.log( 'Icon Url: ' + bungieUrl + characterSummary.data.Response.data.backgroundPath);
+
             })
+             
         });
 
     });
